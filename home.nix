f3907c2172ac94a736da4e274d621d4992b7e99d
Nipa-Dev <nipa.dev05@gmail.com>
2025-11-30 18:58:46 +0200

    Fix old syntax

--- home.nix
+++ home.nix
@@ -25,9 +25,8 @@
 
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
-      yzhang.markdown-all-in-one
       ms-python.python
     ];
   };
